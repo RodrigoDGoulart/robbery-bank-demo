131fb21fdcd356ef64f8452da4ac6acb008cb2fa
RodrigoDGoulart <rodinizgoulart@gmail.com>
2026-05-07 15:47:39 -0300

    feat: convert project to ts & add game board and buttons

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import './App.css'
+import JackpotButtons from './components/JackpotButtons'
+
+const staticImagePath = '/static_images'
+
+function App() {
+  const handleLeftGreenButton = () => {}
+  const handleRedButton = () => {}
+  const handleRightGreenButton = () => {}
+
+  return (
+    <div className="app-shell">
+      <div className="game-board-container">
+        <img
+          className="grid-board"
+          src={`${staticImagePath}/grid-board.png`}
+          alt="Jackpot grid board"
+        />
+        
+        <JackpotButtons
+          onLeftGreenClick={handleLeftGreenButton}
+          onRedClick={handleRedButton}
+          onRightGreenClick={handleRightGreenButton}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default App
